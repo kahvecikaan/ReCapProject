6273f78d3a8949fd6dfc9b9e9ab466a9b296549b
Kaan Kahveci <kaankahveci29@outlook.com>
2023-09-02 18:52:42 +0300

    Refactor and update existing entities

--- Entities/Concrete/Brand.cs
+++ Entities/Concrete/Brand.cs
@@ -5,5 +5,5 @@ namespace Entities.Concrete;
 public class Brand : IEntity
 {
     public int BrandId { get; set; }
-    public string? BrandName { get; set; }
+    public string BrandName { get; set; } = string.Empty;
 }
