f759e2608fa15a15c52e0a6b9dad742d2ec59d21
Kaan Kahveci <kaankahveci29@outlook.com>
2023-09-02 21:06:54 +0300

    Add descriptions for database schema

--- Entities/Concrete/Customer.cs
+++ Entities/Concrete/Customer.cs
@@ -2,6 +2,8 @@ using Core.Entities;
 
 namespace Entities.Concrete;
 
+// The design implies that the Customer entity is an extension of the User entity.
+// Every customer is a user, but not every user is necessarily a customer.
 public class Customer : IEntity
 {
     public int CustomerId { get; set; }
