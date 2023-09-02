ef9841d78aaefe0744decd59093ccbc254026a1e
Kaan Kahveci <kaankahveci29@outlook.com>
2023-09-02 21:05:43 +0300

    Add new descriptive messages for possible situations

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -7,4 +7,9 @@ public static class Messages
     public const string CarsListed = "Cars listed.";
     public const string CarDeleted = "Car deleted.";
     public const string CarUpdated = "Car updated.";
+    public const string RentalAdded = "Rental added.";
+    public const string RentalDeleted = "Rental deleted.";
+    public const string RentalUpdated = "Rental updated.";
+    public const string RentalsListed = "Rentals listed.";
+    public const string RentalNotAdded = "Rental operation failed because the car has not been returned from a previous rental.";
 }
