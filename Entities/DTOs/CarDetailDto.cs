14919b4ba9d10d7a6f3f9f3371824f7607e80a56
Kaan Kahveci <kaankahveci29@outlook.com>
2023-09-06 14:39:51 +0300

    Small updates related to nullable properties

--- Entities/DTOs/CarDetailDto.cs
+++ Entities/DTOs/CarDetailDto.cs
@@ -5,7 +5,7 @@ namespace Entities.DTOs;
 public class CarDetailDto : IDto
 {
     public string? CarName { get; set; }
-    public string? BrandName { get; set; }
-    public string? ColorName { get; set; }
+    public string BrandName { get; set; } = string.Empty;
+    public string ColorName { get; set; } = string.Empty;
     public int DailyPrice { get; set; }
 }
