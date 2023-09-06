2f515e27c3d1f31e8280294f70678a4ccdd878dc
Kaan Kahveci <kaankahveci29@outlook.com>
2023-09-06 11:42:08 +0300

    Add new constant messages according to the needs

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -13,4 +13,9 @@ public static class Messages
     public const string RentalsListed = "Rentals listed.";
     public const string RentalNotAdded = "Rental operation failed because the car has not been returned from a previous rental.";
     public const string RentalNotAvailable = "Rental operation failed because the car is not available.";
+    public const string BrandAdded = "Brand added.";
+    public const string BrandDeleted = "Brand deleted.";
+    public const string BrandUpdated = "Brand updated.";
+    public const string BrandsListed = "Brands listed.";
+    public const string BrandNameInvalid = "Brand name should be at least 2 characters.";
 }
