8d375e500be48a7fc07a21064d792a9eec5e13cc
Kaan Kahveci <kaankahveci29@outlook.com>
2023-09-26 17:25:17 +0300

    feat(Core): Introduce utility for global service access

---
+++ Core/Utilities/IoC/ServiceTool.cs
@@ -0,0 +1,13 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Core.Utilities.IoC;
+
+public static class ServiceTool
+{
+    public static IServiceProvider ServiceProvider { get; private set; } = null!;
+    public static IServiceCollection Create(IServiceCollection services)
+    {
+        ServiceProvider = services.BuildServiceProvider();
+        return services;
+    }
+}
